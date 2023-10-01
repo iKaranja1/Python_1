a7adbabc51e805ed9259bef501a8976779c0ae7d
Karanja <145833416+iKaranja1@users.noreply.github.com>
2023-10-01 18:01:59 +0300

    years_lived.py

---
+++ leap_year.py
@@ -0,0 +1,11 @@
+#Name: Ian Karanja Kamamo
+#Registration no.: Sct211-0461/2022
+
+age = input("What is your current age? ")
+age_integer = int(age)
+
+months = age_integer*12
+weeks = age_integer*52
+days = age_integer*365
+
+print(f"You have lived for {days} days, {weeks} weeks and {months} months.")
